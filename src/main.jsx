cd8b03ecc895894f80ec408f9f1d22d6c0a68c1a
SumanSaha <sumansahaweb.dev@gmail.com>
2025-11-05 20:30:42 +0530

    Disable console logs when VITE_ENVIRONMENT is not development

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Override console methods if not in development
+if (import.meta.env.VITE_ENVIRONMENT !== 'development') {
+  const noop = () => {};
+  console.log = noop;
+  console.warn = noop;
+  console.error = noop;
+  console.info = noop;
+  console.debug = noop;
+  console.table = noop;
+  console.group = noop;
+  console.groupEnd = noop;
+  console.time = noop;
+  console.timeEnd = noop;
+  console.trace = noop;
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
